fe4c66e947927c5016f623ae23f76f5db87aad39
Hybrid-Punk <84671902+Hybrid-Punk@users.noreply.github.com>
2021-06-02 20:20:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var xs=0;
-var ys=0;
+var xs=400;
+var xe=300;
+var ye;
+var ys=200;
 var xb=0;
 var s=5;
 var b=5;
@@ -12,12 +14,15 @@ function draw() {
   background(220,100,100);
 fill(20);  
 ellipse(xs,ys,50,50);
-var yb=windowHeight-100;
+ yb=10*windowHeight/11;
+ ye=windowHeight/11;
 fill(200);
-rect(xb,yb,150,30);
+/*player rectangle display*/rect(xb,yb,150,30);
+/*enemy rectangle display*/rect(xe,ye,150,30);
 xs=xs+s;
 ys=ys+b;
 
+/*controlling player paddle*/
 if(keyIsPressed==true){
   if(keyCode==LEFT_ARROW){
     xb=xb-10;
@@ -26,17 +31,20 @@ if(keyIsPressed==true){
     xb=xb+10;
   }
 }
+
+/*reflecting from walls*/
 if(ys>=xb && ys<=xb+100){
-  if(xs>=width-50){
+  if(xs>=windowWidth-50){
     s=s*-1;
   }
 }
-if(xs>=width || xs<0){
+if(xs>=windowWidth || xs<0){
   s=s*-1;
 }
-if(ys<0){
-  b=b*-1;
-}
+
+
+
+/*reflecting from paddle and score increase and display*/
 if(ys<windowHeight){
 if(xs>=xb && xs<=xb+150 && ys+25>=yb){
   b=b*-1;
@@ -45,6 +53,7 @@ if(xs>=xb && xs<=xb+150 && ys+25>=yb){
 textSize(100);
 text('Score: ' + score, 200, 200);
 }}
+/*when the ball falls down*/
 else if(ys>windowHeight){
   fill(0, 200, 0);
 textSize(100);
@@ -57,62 +66,15 @@ fill(0, 200, 0);
 textSize(100);
 text('Score: ' + score, 200, 200);
 
-
-/*if ((xs > xb &&
-  xs < xb + 150) &&
-  (ys + (50/2) >= yb)) {
-    b=b*-1;
-}*/
-
-
-
-
-
-
+/*moving the enemy paddle*/
+if(xs>xe+75){
+  xe=xe+5;
 }
-
-
-/*var xball=20;
-var yball=50;
-var diameter=50;
-var xballchange=5;
-var yballchange=5;
-var xpaddle=windowWidth/2;
-var ypaddle=windowHeight-100;
-var paddlewidth=100;
-var paddleheight=20;
-
-var xspeed,yspeed;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+if(xs<xe+75){
+  xe=xe-5;
 }
+/*reflect from enemy paddle*/
+if(xs>=xe && xs<=xe+150 && ys-25<=ye+30){
+  b=b*-1;
 
-function draw() {
-  background(255);
-  fill(255, 0, 255);
-  rect(xpaddle,ypaddle,paddlewidth,paddleheight);
-  fill(255, 0, 255);
-noStroke();
-ellipse(xball,yball, diameter,diameter);
-function keyPressed(){
-  if (keycode===RIGHT_ARROW){
-    xpaddle +=50;
-  }
-  else if (keycode===LEFT_ARROW){
-    xpaddle -=50;
-  }
-  
-  xball +=xballchange;
-  yball +=yballchange;
-  
-if (xball<0+diameter/2 || xball>windowWidth-diameter/2){
-  xballchange *=-1;
-}
-if (yball<0+diameter/2 || yball>windowHeight-diameter/2){
-  yballchange*=-1;
-}
-
-
-}
-}*/
+} 
